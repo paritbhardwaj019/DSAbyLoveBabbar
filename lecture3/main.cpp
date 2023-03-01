7fea2dc7f3bddf95b90b538432562a074a354997
Parit Bhardwaj <paritbhardwaj@outlook.com>
2023-03-01 08:47:52 +0530

    Lecture 3 Completed!

---
+++ lecture3/main.cpp
@@ -0,0 +1,182 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  /*
+    int num;
+    cout << "Enter Number: \n";
+    cin >> num;
+
+    if (num > 0) {
+      cout << num << " is Positive" << endl;
+    } else {
+      cout << num << " is Negative" << endl;
+    }
+  */
+
+  /*
+    int a, b;
+
+    cout << "Enter the Value of a :" << endl;
+    cin >> a;
+
+    cout << "Enter the Value of b :" << endl;
+    cin >> b;
+
+    if ( a > b ) {
+      cout << a << " is Greater than " << b << endl;
+    }
+    if ( b > a ) {
+      cout << b << " is Greater than " << a << endl;
+    }
+  */
+  /*
+    int num;
+    cout << "Enter Number: \n";
+    cin >> num;
+
+    if (num > 0) {
+      cout << num << " is Positive" << endl;
+    } else {
+      if ( num == 0 ) {
+        cout << num << " is Nither Postive nor Negative" << endl;
+      } else {
+        cout << num << " is Negative" << endl;
+      }
+    }
+  */
+  /*
+    int num;
+    cout << "Enter Number: \n";
+    cin >> num;
+
+    if (num > 0) {
+      cout << num << " is Positive" << endl;
+    } else if (num < 0) {
+      cout << num << " is Negative" << endl;
+    } else {
+      cout << num << " is Nither Postive nor Negative" << endl;
+    }
+  */
+
+  /*
+    // Homework Question
+
+    char ch;
+    cout << "Enter Your Letter : " << endl;
+    cin >> ch;
+
+    cout << int(ch) << endl;
+
+    if (int(ch) >= 65 && int(ch) <= 90) {
+      cout << ch << " Is Uppercase" << endl;
+    } else if (int(ch) >= 97 && int(ch) <= 122) {
+      cout << ch << " Is Lowercase" << endl;
+    } else if (int(ch) >= 48 && int(ch) <= 57) {
+      cout << ch << " Is Number" << endl;
+    } else {
+      cout << ch << " Is Another Character" << endl;
+    }
+  */
+
+  /*
+    int num;
+    cout << "Enter Number : " << endl;
+    cin >> num;
+
+    int i = 1;
+
+    while ( i <= num ) {
+      cout << i << endl;
+      i = i + 1;
+    }
+  */
+
+  /*
+    int num;
+    cout << "Enter Number : " << endl;
+    cin >> num;
+
+    int i = 1;
+    int sum = 0;
+
+    while ( i <= num ) {
+      sum = sum + i;
+      i = i + 1;
+    }
+
+    cout << sum << endl;
+
+  */
+  /*
+    // Homework Questions
+    int num;
+    cout << "Enter Number : " << endl;
+    cin >> num;
+
+    int i = 2;
+    int sum = 0;
+
+    while ( i <= num ) {
+      sum = sum + i;
+      i = i + 2;
+    }
+
+    cout << sum << endl;
+
+  */
+
+  /*
+    int num;
+    cout << "Enter Your Number : " << endl;
+    cin >> num;
+
+    int i = 2;
+
+    while (i < num) {
+      if (num % i == 0) {
+        cout << "Not Prime" << endl;
+      } else {
+        cout << "is Not Prime" << endl;
+      }
+      i = i + 1;
+    }
+  */
+
+  /*
+    int num;
+    cout << "Enter Number of Rows : " << endl;
+    cin >> num;
+
+    int i = 1;
+    while (i <= num) {
+      int j = 1;
+      while (j <= num) {
+        cout << "*";
+        j = j + 1;
+      }
+      cout << endl;
+      i = i + 1;
+    }
+  */
+
+  /*
+
+    int num;
+    cout << "Enter Number of Rows : " << endl;
+    cin >> num;
+
+    int i = 1;
+
+    while (i <= num) {
+      int j = 1;
+      while (j <= num) {
+        cout << i;
+        j = j + 1;
+      }
+      cout << endl;
+      i = i + 1;
+    }
+
+  */
+}
